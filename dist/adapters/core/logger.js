2375a00eaefef405d7e368ad1dc176319fcc4863
stenreen <130090053+stenreen@users.noreply.github.com>
2026-04-12 19:08:18 +0200

    Delete dist/adapters/core directory

--- dist/adapters/core/logger.js
+++
@@ -1,6 +0,0 @@
-export function logInfo(message, meta) {
-    console.log(JSON.stringify({ level: "info", message, meta }));
-}
-export function logError(message, meta) {
-    console.error(JSON.stringify({ level: "error", message, meta }));
-}
